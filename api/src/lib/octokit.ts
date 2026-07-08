67fd6dbddbdc98117d57d4798465ab2a941a45c1
Yamin Hossain <168386743+yamin-H@users.noreply.github.com>
2026-07-08 08:34:46 +0600

    Refactor privateKey initialization for flexibility

--- api/src/lib/octokit.ts
+++ api/src/lib/octokit.ts
@@ -2,10 +2,8 @@ import { createAppAuth } from '@octokit/auth-app'
 import { Octokit } from '@octokit/rest'
 import fs from 'fs'
 
-const privateKey = fs.readFileSync(
-    process.env.GITHUB_PRIVATE_KEY_PATH!,
-    'utf8'
-);
+const privateKey = process.env.GITHUB_PRIVATE_KEY ||
+  fs.readFileSync(process.env.GITHUB_PRIVATE_KEY_PATH!, 'utf8')
 
 const appAuth = createAppAuth({
     appId: process.env.GITHUB_APP_ID!,

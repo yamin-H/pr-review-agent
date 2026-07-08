e16904aca15d38a8fd6dd14bacbc3679ef76623c
Yamin Hossain <yamindr3@gmail.com>
2026-07-08 07:58:13 +0600

    change the redis file

--- api/src/lib/redis.ts
+++ api/src/lib/redis.ts
@@ -1,7 +1,12 @@
-import Redis from 'bullmq/node_modules/ioredis'
+import { Redis } from "ioredis"
+
+const isUpstash = process.env.REDIS_URL?.startsWith('rediss://')
 
 const connection = new Redis(process.env.REDIS_URL || 'redis://localhost:6379', {
-    maxRetriesPerRequest: null 
+    maxRetriesPerRequest: null,
+    ...(isUpstash && {
+        tls: {}
+    })
 });
 
 connection.on('connect', () => console.log('Redis connected'))
